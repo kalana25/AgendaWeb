5fa85dade59f2ecbec3c7277df4eaea9f14463ff
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-08-10 11:08:40 +0100

    get all resource profile use case

---
+++ AgendaWeb.UseCases/General/ResourceProfiles/GetAllResourceProfile/GetAllResourceProfileUseCase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AgendaWeb.Models;
+using System.Threading.Tasks;
+using AgendaWeb.Core.Interfaces;
+using AgendaWeb.Repositories;
+
+namespace AgendaWeb.UseCases.General.ResourceProfiles.GetAllResourceProfile
+{
+    public class GetAllResourceProfileUseCase : UseCase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public GetAllResourceProfileUseCase(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<ResourceProfile>>Execute()
+        {
+            return await this.unitOfWork.ResourceProfile.GetAll();
+        }
+    }
+}
