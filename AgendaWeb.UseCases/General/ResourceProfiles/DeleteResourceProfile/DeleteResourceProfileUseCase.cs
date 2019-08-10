0d87b484701d16851d2976e3b1b3e4ffbe21c63b
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-08-10 11:17:19 +0100

    resource profile delete usecase.

---
+++ AgendaWeb.UseCases/General/ResourceProfiles/DeleteResourceProfile/DeleteResourceProfileUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AgendaWeb.Core.Interfaces;
+using AgendaWeb.Repositories;
+using AgendaWeb.Models;
+using System.Threading.Tasks;
+using AgendaWeb.UseCases.DTO;
+using AutoMapper;
+
+namespace AgendaWeb.UseCases.General.ResourceProfiles.DeleteResourceProfile
+{
+    public class DeleteResourceProfileUseCase : UseCase
+    {
+        public int Id { get; set; }
+
+        private readonly IUnitOfWork unitOfWork;
+
+        public DeleteResourceProfileUseCase(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Execute()
+        {
+            var resourceProfile = await unitOfWork.ResourceProfile.Get(Id);
+            unitOfWork.ResourceProfile.Remove(resourceProfile);
+            var result = unitOfWork.Complete();
+            return resourceProfile.Id;
+        }
+    }
+}
