1ea398fdd888928e1c5b37c326e9ec30ecb0c9fa
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-08-10 11:14:17 +0100

    get resource profile usecase

---
+++ AgendaWeb.UseCases/General/ResourceProfiles/GetResourceProfile/GetResourceProfileUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AgendaWeb.Core.Interfaces;
+using AgendaWeb.Repositories;
+using AgendaWeb.Models;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace AgendaWeb.UseCases.General.ResourceProfiles.GetResourceProfile
+{
+    public class GetResourceProfileUseCase:UseCase
+    {
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public int Id { get; set; }
+
+        public GetResourceProfileUseCase(
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<ResourceProfile> Execute()
+        {
+            return await unitOfWork.ResourceProfile.Get(this.Id);
+        }
+    }
+}
