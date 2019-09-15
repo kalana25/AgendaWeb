854d6170bc085d4bdcd78334b78fbfa7a4f4dcc7
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-09-15 22:49:37 +0530

    Get collaborator usecase

---
+++ AgendaWeb.UseCases/General/Collaborators/GetCollaboratorWithFullInfo/GetCollaboratorWithFullInfoUseCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using AgendaWeb.UseCases.DTO;
+using AgendaWeb.Models;
+using AgendaWeb.Repositories;
+using System.Threading.Tasks;
+using AgendaWeb.Core.Interfaces;
+
+namespace AgendaWeb.UseCases.General.Collaborators.GetCollaboratorWithFullInfo
+{
+    public class GetCollaboratorWithFullInfoUseCase: UseCase
+    {
+        public int Id { get; set; }
+
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public GetCollaboratorWithFullInfoUseCase(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<CollaboratorWithFullInfoDTO> Execute()
+        {
+            Collaborator collaborator = await this.unitOfWork.Collaborators.GetCollaboratorWithFullInfor(this.Id);
+            CollaboratorWithFullInfoDTO result = mapper.Map<Collaborator, CollaboratorWithFullInfoDTO>(collaborator);
+            return result;
+        }
+    }
+}
