3d1041500f7e4b0bf8a004dcf9a8d2169cd310b5
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-09-15 23:10:27 +0530

    update collaborator usecase

---
+++ AgendaWeb.UseCases/General/Collaborators/UpdateCollaborator/UpdateCollaboratorUseCase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using AgendaWeb.Core.Interfaces;
+using AgendaWeb.Repositories;
+using AgendaWeb.Models;
+using AgendaWeb.UseCases.DTO;
+
+namespace AgendaWeb.UseCases.General.Collaborators.UpdateCollaborator
+{
+    public class UpdateCollaboratorUseCase : UseCase
+    {
+        public int Id { get; set; }
+        public CollaboratorUpdateDTO DTO { get; set; }
+
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public UpdateCollaboratorUseCase(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<Collaborator> Execute()
+        {
+            var existingCollaborator = await unitOfWork.Collaborators.Get(Id);
+            Collaborator updatedCollaborator = this.mapper.Map<CollaboratorUpdateDTO, Collaborator>(DTO, existingCollaborator);
+            var existingAddress = await unitOfWork.Addresses.Get(DTO.Address.Id);
+            Address address = this.mapper.Map<AddressInfoDTO, Address>(DTO.Address, existingAddress);
+            var existingCommunication = await unitOfWork.Communications.Get(DTO.Communication.Id);
+            Communication communication = this.mapper.Map<CommunicationInfoDTO, Communication>(DTO.Communication, existingCommunication);
+            int result = await unitOfWork.Complete();
+            return updatedCollaborator;
+        }
+    }
+}
