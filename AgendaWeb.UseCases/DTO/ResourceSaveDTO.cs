853392639bc39a0bc0783c364e492bac44bbdb85
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-09-19 22:02:07 +0530

    resource save usecase

---
+++ AgendaWeb.UseCases/DTO/ResourceSaveDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AgendaWeb.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgendaWeb.UseCases.DTO
+{
+    public class ResourceSaveDTO
+    {
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(250)]
+        public string Description { get; set; }
+
+        [MaxLength(5)]
+        public string Abbreviation { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public bool Plannable { get; set; }
+
+        [Required]
+        public ResourceCategory CategoryId { get; set; }
+
+        [Required]
+        public int StyleId { get; set; }
+
+        [Required]
+        public int CollaboratorId { get; set; }
+
+        public List<int> ResourceProfileIds { get; set; }
+    }
+}
---
+++ AgendaWeb.UseCases/General/Resources/SaveResource/SaveResourceUseCase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using AgendaWeb.Models;
+using System.Threading.Tasks;
+using AgendaWeb.UseCases.DTO;
+using AutoMapper;
+using AgendaWeb.Core.Interfaces;
+using AgendaWeb.Repositories;
+namespace AgendaWeb.UseCases.General.Resources.SaveResource
+{
+    public class SaveResourceUseCase:UseCase
+    {
+        public ResourceSaveDTO DTO { get; set; }
+
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public SaveResourceUseCase(
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<Resource> Execute()
+        {
+            Resource resource = this.mapper.Map<ResourceSaveDTO, Resource>(this.DTO);
+            var resourceResourceProfile = DTO.ResourceProfileIds.Select(x => new ResourceResourceProfile() { ResourceProfileId = x }).ToList();
+            resource.ResourceResourceProfiles = resourceResourceProfile;
+            this.unitOfWork.Resources.Add(resource);
+            await this.unitOfWork.Complete();
+            return resource;
+        }
+    }
+}
