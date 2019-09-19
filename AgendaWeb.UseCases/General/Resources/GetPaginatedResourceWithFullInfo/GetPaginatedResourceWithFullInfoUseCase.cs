754231fe5376b97999991e88dda347af644c62dd
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-09-19 20:58:19 +0530

    get paginated resource with full info usecase

---
+++ AgendaWeb.UseCases/General/Resources/GetPaginatedResourceWithFullInfo/GetPaginatedResourceWithFullInfoUseCase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using AgendaWeb.UseCases.DTO;
+using AgendaWeb.Models;
+using AgendaWeb.Repositories;
+using System.Threading.Tasks;
+using AgendaWeb.Core.Interfaces;
+
+namespace AgendaWeb.UseCases.General.Resources.GetPaginatedResourceWithFullInfo
+{
+    public class GetPaginatedResourceWithFullInfoUseCase:UseCase
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+
+        public readonly IMapper mapper;
+        public readonly IUnitOfWork unitOfWork;
+
+        public GetPaginatedResourceWithFullInfoUseCase(
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<ResourceWithFullInfoDTO>> Execute()
+        {
+            var resources = await this.unitOfWork.Resources.GetPaginatedResourceWithFullInfo(PageIndex, PageSize);
+            IEnumerable<ResourceWithFullInfoDTO> result = mapper.Map<IEnumerable<Resource>, IEnumerable<ResourceWithFullInfoDTO>>(resources);
+            return result;
+        }
+    }
+}
